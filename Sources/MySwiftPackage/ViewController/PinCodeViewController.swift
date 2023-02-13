137a5c851e14742b585ae11ce46ba38a09e9ea99
pariyakorn kayjarit <pariyakon12@gmail.com>
2023-02-14 03:11:18 +0700

    add uiview

---
+++ Sources/MySwiftPackage/ViewController/PinCodeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PinCodeViewController.swift
+//  
+//
+//  Created by pariyakorn kayjarit on 14/2/2566 BE.
+//
+
+import UIKit
+
+class PinCodeViewController: UIViewController {
+    
+    public static let storyboardPnCode = UIStoryboard(name: "PinCode", bundle: Bundle.module).instantiateInitialViewController()!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
