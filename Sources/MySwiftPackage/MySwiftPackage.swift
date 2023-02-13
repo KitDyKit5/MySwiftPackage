5d44285bd30fadbb8392e62f5b8d4380c91115d9
pariyakorn kayjarit <pariyakon12@gmail.com>
2023-02-13 16:45:26 +0700

    edit

--- Sources/MySwiftPackage/MySwiftPackage.swift
+++ Sources/MySwiftPackage/MySwiftPackage.swift
@@ -4,6 +4,10 @@ import CommonCrypto
 public struct MyLibrary {
     var text = "Hello, World"
     
+    public init() {
+        
+    }
+    
     public func HelloToYou() -> String {
         return "Hello To You!"
     }
