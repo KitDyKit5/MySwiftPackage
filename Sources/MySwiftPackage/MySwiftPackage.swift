bc4146689f83d2f0aa7eb224d650a5842cd149a3
pariyakorn kayjarit <pariyakon12@gmail.com>
2023-02-13 16:09:59 +0700

    edit

--- Sources/MySwiftPackage/MySwiftPackage.swift
+++ Sources/MySwiftPackage/MySwiftPackage.swift
@@ -1,6 +1,38 @@
-public struct MySwiftPackage {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import CommonCrypto
 
-    public init() {
+public struct MyLibrary {
+    var text = "Hello, World"
+    
+    public func HelloToYou() -> String {
+        return "Hello To You!"
     }
 }
+
+//public enum Sha256{
+//    case sha256
+//
+//    public func sha256(data: String) -> String? {
+//        var digest = Data(count: Int(CC_SHA256_DIGEST_LENGTH))
+//        _ = digest.withUnsafeMutableBytes { (digestBytes) in
+//            data.data(using: .utf8)!.withUnsafeBytes { (stringBytes) in
+//                CC_SHA256(stringBytes, CC_LONG(data.count), digestBytes)
+//            }
+//        }
+//        let sha256 = digest.map { String(format: "%02hhx", $0) }.joined()
+//        return sha256
+//    }
+//}
+//
+//public protocol Sha256Kit {
+//    func sha256() -> String?
+//}
+//
+//extension String: Sha256Kit{
+//    public func sha256() -> String? {
+//        return Sha256.sha256.sha256(data: self)
+//    }
+//
+//}
+
+
